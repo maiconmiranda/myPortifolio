3886329f7a6f241de367070a5f5781697e91bf04
Maicon Miranda <forcoddingmatters@gmail.com>
2021-07-23 17:13:26 +1000

    add all code blocks and temporally removed copy button

---
+++ brewCoffee.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+  hljs.initHighlightingOnLoad();
+
+  //   const codeBlock = document.getElementById("code");
+  //   const copyButton = document.getElementById("copy__button");
+  //   const copySuccess = document.getElementById("copy__success");
+
+  //   const copyTextHandler = () => {
+  //     const text = codeBlock.innerText;
+  //     navigator.clipboard.writeText(text).then(
+  //       () => {
+  //         copySuccess.classList.add("show-message");
+  //         setTimeout(() => {
+  //           copySuccess.classList.remove("show-message");
+  //         }, 2500);
+  //       },
+  //       () => {
+  //         console.log("Error writing to the clipboard");
+  //       }
+  //     );
+  //   };
+
+  //   copyButton.addEventListener("click", copyTextHandler());
+});
